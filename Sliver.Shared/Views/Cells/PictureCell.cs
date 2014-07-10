ca11e71d85a50d97dfa93428cc05e6bd88d33476
Macie Hummer <macie.hummer@fireflylogic.com>
2014-07-10 14:56:20 -0500

    got the bindings to work in custom view cell and the pictures showgit status

--- Sliver.Shared/Views/Cells/PictureCell.cs
+++ Sliver.Shared/Views/Cells/PictureCell.cs
@@ -1,43 +1,90 @@
 ﻿using System;
 using Xamarin.Forms;
+using sliver;
 
 namespace Sliver.Shared
 {
-	public class PictureCell: ViewCell
+	public class PictureCell : ViewCell
 	{
 		public PictureCell ()
 		{
 			// create the layout for the Picture Cell
 			var relativeLayout = new RelativeLayout ();
 
-			// create the UI elements in the Picture Cell
-			Image pic = new Image
+			/* create the UI elements in the Picture Cell */
+			// create picture 
+			var pic = new Image
 			{
 				VerticalOptions = LayoutOptions.CenterAndExpand,
-				HorizontalOptions = LayoutOptions.CenterAndExpand
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
 			};
 
-			Label creatorLabel = new Label 
+			// create creator label 
+			var creatorLabel = new Label 
 			{
 				VerticalOptions = LayoutOptions.Center,
 				HorizontalOptions = LayoutOptions.StartAndExpand,
 				TextColor = Color.White,
-				Font = Font.BoldSystemFontOfSize(15)
+				Font = Font.BoldSystemFontOfSize(15),
 			};
 
-			Label dateTimeLabel = new Label 
+			// create date and time label
+			var dateTimeLabel = new Label 
 			{
 				VerticalOptions = LayoutOptions.Center,
 				HorizontalOptions = LayoutOptions.EndAndExpand,
 				TextColor = Color.White,
-				Font = Font.SystemFontOfSize(15)
+				Font = Font.SystemFontOfSize(15),
 			};
 
-			// set property bindings
 			pic.SetBinding (Image.SourceProperty, "ImageUrl");
 			creatorLabel.SetBinding (Label.TextProperty, "Creator");
 			dateTimeLabel.SetBinding (Label.TextProperty, "DateAndTimeString");
 
+			/* - * end UI elements in Picture Cell * - */
+
+			// add children to relativeLayout
+			relativeLayout.Children.Add ((Image)pic, 
+				null, 
+				null, 
+				Constraint.RelativeToParent ((parent) => 
+					{
+						return parent.Width;
+					}), 
+				Constraint.RelativeToParent ((parent) => 
+					{
+						return parent.Height;
+					})
+			);
+
+			var stackLayout = new StackLayout 
+			{
+				BackgroundColor = Color.FromRgba(255, 255, 255, 50),
+				Orientation = StackOrientation.Horizontal,
+				VerticalOptions = LayoutOptions.EndAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				HeightRequest = 100,
+				Children = 
+				{
+					(Label)creatorLabel,
+					(Label)dateTimeLabel
+				}
+			};
+
+			relativeLayout.Children.Add (stackLayout, 
+				null, 
+				Constraint.RelativeToView ((Image)pic, (parent, sibling) => 
+					{
+						return sibling.Height - 100;
+					}), 
+				Constraint.RelativeToParent ((parent) => 
+					{
+						return parent.Width;
+					}), 
+				null
+			);
+
+			this.View = relativeLayout;
 		}
 	}
 }
--- Sliver.Shared/Views/PicturesAtPlacePage.cs
+++ Sliver.Shared/Views/PicturesAtPlacePage.cs
@@ -5,8 +5,13 @@ using Sliver.Shared;
 
 namespace sliver
 {
-	public class PicturesAtPlacePage: ContentPage
+	public class PicturesAtPlacePage : ContentPage
 	{
+		private PictureViewModel ViewModel
+		{
+			get{ return BindingContext as PictureViewModel; }
+		}
+
 		public PicturesAtPlacePage ()
 		{
 			// set page title
@@ -16,7 +21,7 @@ namespace sliver
 			BackgroundColor = Color.Silver;
 
 			// set the view model
-			var viewModel = new PictureViewModel ();
+			BindingContext = new PictureViewModel ();
 
 			/* create UI of page */
 			// create the list view
@@ -25,19 +30,15 @@ namespace sliver
 				VerticalOptions = LayoutOptions.CenterAndExpand,
 				HorizontalOptions = LayoutOptions.CenterAndExpand
 			};
-			pictureListView.ItemsSource = viewModel.pictureList;
-
+			pictureListView.ItemsSource = ViewModel.pictureList;
+					
 			// create and customize the cell(s)
 			var cell = new DataTemplate (typeof(PictureCell));
 			pictureListView.ItemTemplate = cell;
-
+		
 			// create refresh control -- IF POSSIBLE WITH XAMARIN FORMS
 
 			/* Handle Events */
-			pictureListView.ItemTapped += (object sender, ItemTappedEventArgs e) => 
-			{
-				Console.WriteLine("--> picture tapped!");
-			};
 
 			// add padding to the UI -- accomodate the iOS status bar
 			Padding = new Thickness (0, Device.OnPlatform (20, 0, 0), 0, 0);
