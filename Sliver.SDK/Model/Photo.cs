2ffdf4023d6ad502e2ce665947a34fe35f31c19b
Ben Henderson <ben.henderson@fireflylogic.com>
2014-07-14 09:06:04 -0500

    Added PhotoService

--- Sliver.SDK/Model/Photo.cs
+++ Sliver.SDK/Model/Photo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+//using Newtonsoft.Json;
 
 namespace Sliver.SDK.Model
 {
@@ -8,10 +9,19 @@ namespace Sliver.SDK.Model
 		{
 		}
 
+//		[JsonProperty(PropertyName = "creator")]
 		public string Creator { get; set; }
+
+//		[JsonProperty(PropertyName = "url")]
 		public string Url { get; set; }
+
+//		[JsonProperty(PropertyName = "timeTaken")]
 		public DateTime TimeTaken { get; set; }
+
+//		[JsonProperty(PropertyName = "locationLatitude")]
 		public double LocationLatitude { get; set; }
+
+//		[JsonProperty(PropertyName = "locationLonitude")]
 		public double LocationLongtude { get; set; }
 	}
 }
--- Sliver.SDK/Service/PhotoService.cs
+++ Sliver.SDK/Service/PhotoService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Sliver.SDK.Model;
+//using Microsoft.WindowsAzure.MobileServices;
 
 namespace Sliver.SDK.Service
 {
@@ -15,9 +16,22 @@ namespace Sliver.SDK.Service
 			throw new NotImplementedException ();
 		}
 
-		public void UploadPhoto (string creator, DateTime timeTaken, double latitude, double longitude, string photoPath)
+		public async void UploadPhoto (string creator, DateTime timeTaken, double latitude, double longitude, string photoPath)
 		{
-			throw new NotImplementedException ();
+			var photo = new Photo () {
+				Creator = creator,
+				TimeTaken = timeTaken,
+				LocationLatitude = latitude,
+				LocationLongtude = longitude,
+				Url = "",
+			};
+
+//			var sliverClient = new Microsoft.WindowsAzure.MobileServices.MobileServiceClient (
+//                   "https://sliver.azure-mobile.net/",
+//                   "qxvnFuoFNMmBHUPBuVqqLcmJhpeJqn13"
+//               );
+//
+//			await sliverClient.GetTable<Photo>().InsertAsync(photo);
 		}
 	}
 }
